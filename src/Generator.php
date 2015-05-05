a3ba1d8892c821e021c625cf4fe03f878b2efb95
Darius Matulionis <darius@matulionis.lt>
2015-05-05 09:25:24 +0300

    2.0 swagger

--- src/Generator.php
+++ src/Generator.php
@@ -1,6 +1,5 @@
 <?php namespace Darkaonline\L5Swagger;
 
-use Swagger\Swagger;
 use Config;
 use File;
 
@@ -14,34 +13,11 @@ class Generator {
                 File::deleteDirectory($docDir);
             }
             File::makeDirectory($docDir);
-            $defaultBasePath = Config::get('l5-swagger.default-base-path');
-            $defaultApiVersion = Config::get('l5-swagger.default-api-version');
-            $defaultSwaggerVersion = Config::get('l5-swagger.default-swagger-version');
             $excludeDirs = Config::get('l5-swagger.excludes');
-            $swagger = new Swagger($appDir, $excludeDirs);
-            $resourceList = $swagger->getResourceList(array(
-                'output' => 'array',
-                'apiVersion' => $defaultApiVersion,
-                'swaggerVersion' => $defaultSwaggerVersion,
-            ));
-            $resourceOptions = array(
-                'output' => 'json',
-                'defaultSwaggerVersion' => $resourceList['swaggerVersion'],
-                'defaultBasePath' => $defaultBasePath
-            );
-            $output = array();
-            foreach ($swagger->getResourceNames() as $resourceName) {
-                $json = $swagger->getResource($resourceName, $resourceOptions);
-                $resourceName = str_replace(DIRECTORY_SEPARATOR, '-', ltrim($resourceName, DIRECTORY_SEPARATOR));
-                $output[$resourceName] = $json;
-            }
+            $swagger = \Swagger\scan($appDir, $excludeDirs);
+
             $filename = $docDir . '/api-docs.json';
-            file_put_contents($filename, Swagger::jsonEncode($resourceList, true));
-            foreach ($output as $name => $json) {
-                $name = str_replace(DIRECTORY_SEPARATOR, '-', ltrim($name, DIRECTORY_SEPARATOR));
-                $filename = $docDir . '/'.$name . '.json';
-                file_put_contents($filename, $json);
-            }
+            $swagger->saveAs($filename);
         }
     }
 }
