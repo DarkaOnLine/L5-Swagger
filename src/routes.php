01989338305ca90833cbe337eb09e360769db640
Darius Matulionis <darius@matulionis.lt>
2016-04-19 02:51:05 -0400

    Applied fixes from StyleCI

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,5 @@
 <?php
 
-$router->any(config('l5-swagger.routes.docs') . '/{page?}', '\L5Swagger\Http\Controllers\SwaggerController@docs');
+$router->any(config('l5-swagger.routes.docs').'/{page?}', '\L5Swagger\Http\Controllers\SwaggerController@docs');
 
 $router->get(config('l5-swagger.routes.api'), '\L5Swagger\Http\Controllers\SwaggerController@api');
