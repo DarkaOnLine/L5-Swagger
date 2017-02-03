a1424621acfe5252b0dd9dd72be4e8cc4dd8c1f7
Darius Matulionis <darius@matulionis.lt>
2017-02-03 08:22:43 +0200

    middleware fix

--- src/routes.php
+++ src/routes.php
@@ -2,12 +2,12 @@
 
 $router->any(config('l5-swagger.routes.docs').'/{jsonFile?}', [
     'as' => 'l5-swagger.docs',
-    'middleware' => config('l5-swagger.middleware.docs', []),
+    'middleware' => config('l5-swagger.routes.middleware.docs', []),
     'uses' => '\L5Swagger\Http\Controllers\SwaggerController@docs',
 ]);
 
 $router->get(config('l5-swagger.routes.api'), [
     'as' => 'l5-swagger.api',
-    'middleware' => config('l5-swagger.middleware.api', []),
+    'middleware' => config('l5-swagger.routes.middleware.api', []),
     'uses' => '\L5Swagger\Http\Controllers\SwaggerController@api',
 ]);
