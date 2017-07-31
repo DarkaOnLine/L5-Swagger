7b27f89b0bed92e98dbb728588ecd7ec2a979c94
FISCHERHOMES\jashmore <jashmore@fischerhomes.com>
2017-07-31 11:48:50 -0400

    Add middleware options

--- src/routes.php
+++ src/routes.php
@@ -14,10 +14,12 @@ $router->get(config('l5-swagger.routes.api'), [
 
 $router->get(config('l5-swagger.routes.docs').'/asset/{asset}', [
     'as' => 'l5-swagger.asset',
+    'middleware' => config('l5-swagger.routes.middleware.asset', []),
     'uses' => '\L5Swagger\Http\Controllers\SwaggerAssetController@index',
 ]);
 
 $router->get(config('l5-swagger.routes.oauth2_callback'), [
     'as' => 'l5-swagger.oauth2_callback',
+    'middleware' => config('l5-swagger.routes.middleware.oauth2_callback', []),
     'uses' => '\L5Swagger\Http\Controllers\SwaggerController@oauth2Callback',
 ]);
