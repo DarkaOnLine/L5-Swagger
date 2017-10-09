d77b3a4d2a09b275ffb70d8c00796bfdb8e71ee8
Jeff Puckett <jeff@jeffpuckett.com>
2017-10-09 05:26:15 +0000

    fix route precedence (#90)

--- src/routes.php
+++ src/routes.php
@@ -1,15 +1,15 @@
 <?php
 
-$router->any(config('l5-swagger.routes.docs').'/{jsonFile?}', [
-    'as' => 'l5-swagger.docs',
-    'middleware' => config('l5-swagger.routes.middleware.docs', []),
-    'uses' => '\L5Swagger\Http\Controllers\SwaggerController@docs',
-]);
-
 $router->get(config('l5-swagger.routes.api'), [
     'as' => 'l5-swagger.api',
     'middleware' => config('l5-swagger.routes.middleware.api', []),
     'uses' => '\L5Swagger\Http\Controllers\SwaggerController@api',
+]);
+
+$router->any(config('l5-swagger.routes.docs').'/{jsonFile?}', [
+    'as' => 'l5-swagger.docs',
+    'middleware' => config('l5-swagger.routes.middleware.docs', []),
+    'uses' => '\L5Swagger\Http\Controllers\SwaggerController@docs',
 ]);
 
 $router->get(config('l5-swagger.routes.docs').'/asset/{asset}', [
