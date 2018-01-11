dfc85b7b74d3e654db64dc74a722b2a9d341afe0
Darius Matulionis <darius@matulionis.lt>
2018-01-11 14:38:10 +0200

    add assets checksum for cache busting. Fixes #91

--- src/helpers.php
+++ src/helpers.php
@@ -58,6 +58,6 @@ if (! function_exists('l5_swagger_asset')) {
             throw new L5SwaggerException(sprintf('Requested L5 Swagger asset file (%s) does not exists', $asset));
         }
 
-        return route('l5-swagger.asset', $asset);
+        return route('l5-swagger.asset', $asset).'?v='.md5_file($file);
     }
 }
