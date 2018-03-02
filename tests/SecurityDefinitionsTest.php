186b891abb0b3d57239f5fdcc35cbef7f4451b46
Darius Matulionis <darius@matulionis.lt>
2018-03-02 13:28:53 +0200

    OpenAPI 3.0 Support For SecurityDefinitions

---
+++ tests/SecurityDefinitionsTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests;
+
+use L5Swagger\Generator;
+
+class SecurityDefinitionsTest extends TestCase
+{
+    /** @test */
+    public function canGenerateApiJsonFileWithSecurityDefinition()
+    {
+        if ($this->isOpenApi()) {
+            $this->markTestSkipped('only for openApi 2.0');
+        }
+        $this->setAnnotationsPath();
+
+        $cfg = config('l5-swagger');
+        $security = [
+            'new_api_key_securitye' => [
+                'type' => 'apiKey',
+                'name' => 'api_key_name',
+                'in' => 'query',
+            ],
+        ];
+        $cfg['security'] = $security;
+        $cfg['swagger_version'] = '2.0';
+        config(['l5-swagger' => $cfg]);
+
+        tap(new Generator)->generateDocs();
+
+        $this->assertTrue(file_exists($this->jsonDocsFile()));
+
+        $this->get(route('l5-swagger.docs'))
+             ->assertSee('new_api_key_securitye')
+             ->assertJsonFragment($security)
+             ->isOk();
+    }
+
+    /** @test */
+    public function canGenerateApiJsonFileWithSecurityDefinitionOpenApi3()
+    {
+        if (! $this->isOpenApi()) {
+            $this->markTestSkipped('only for openApi 3.0');
+        }
+        $this->setAnnotationsPath();
+
+        $cfg = config('l5-swagger');
+        $security = [
+            'new_api_key_securitye' => [
+                'type' => 'apiKey',
+                'name' => 'api_key_name',
+                'in' => 'query',
+            ],
+        ];
+        $cfg['security'] = $security;
+        $cfg['swagger_version'] = '3.0';
+        config(['l5-swagger' => $cfg]);
+
+        tap(new Generator)->generateDocs();
+
+        $this->assertTrue(file_exists($this->jsonDocsFile()));
+
+        $this->get(route('l5-swagger.docs'))
+             ->assertSee('new_api_key_securitye')
+             ->assertJsonFragment($security)
+             ->isOk();
+    }
+}
--- tests/storage/annotations/Swagger/Anotations.php
+++ tests/storage/annotations/Swagger/Anotations.php
@@ -16,6 +16,15 @@
  * )
  */
 
+/**
+ * @SWG\SecurityScheme(
+ *   securityDefinition="passport",
+ *   type="oauth2",
+ *   tokenUrl="/oauth/token",
+ *   flow="password",
+ *   scopes={}
+ * )
+ */
 
 /**
  * @SWG\Get(
