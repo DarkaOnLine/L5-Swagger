cbb4a8dc6f183291871a1826cc00cc380dfcdd6b
Darius Matulionis <darius.matulionis@supermetrics.com>
2020-05-07 12:04:10 +0300

    Update Middleware/Config

--- src/Http/Middleware/Config.php
+++ src/Http/Middleware/Config.php
@@ -4,16 +4,30 @@ namespace L5Swagger\Http\Middleware;
 
 use Closure;
 use L5Swagger\ConfigFactory;
+use L5Swagger\Exceptions\L5SwaggerException;
 
 class Config
 {
+    /**
+     * @var ConfigFactory
+     */
     private $configFactory;
 
+    /**
+     * Config constructor.
+     * @param ConfigFactory $configFactory
+     */
     public function __construct(ConfigFactory $configFactory)
     {
         $this->configFactory = $configFactory;
     }
 
+    /**
+     * @param $request
+     * @param Closure $next
+     * @return mixed
+     * @throws L5SwaggerException
+     */
     public function handle($request, Closure $next)
     {
         $actions = $request->route()->getAction();
