27ea25896c9f6357d755e8cb6fecb49188c44582
Darius Matulionis <darius.matulionis@supermetrics.com>
2020-08-18 09:24:56 +0300

    Fix requestInterceptor

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -81,9 +81,9 @@ window.onload = function() {
     validatorUrl: {!! isset($validatorUrl) ? '"' . $validatorUrl . '"' : 'null' !!},
     oauth2RedirectUrl: "{{ route('l5-swagger.oauth2_callback') }}",
 
-    requestInterceptor: function() {
-      this.headers['X-CSRF-TOKEN'] = '{{ csrf_token() }}';
-      return this;
+    requestInterceptor: function(request) {
+      request.headers['X-CSRF-TOKEN'] = '{{ csrf_token() }}';
+      return request;
     },
 
     presets: [
