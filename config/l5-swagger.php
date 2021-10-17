a10b484c801f5a9fd63d160bc5d138f613f4d279
seyfi <seyfipouria@gmail.com>
2021-10-17 14:43:23 +0330

    fix array style

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -245,12 +245,13 @@ return [
                  * */
                 'doc_expansion' => 'list',
             ],
+
             'authorization' => [
                 /*
                  *  If set to true, it persists authorization data, and it would not be lost on browser close/refresh
                  */
                 'persist_authorization' => true,
-                ]
+            ],
         ],
         /*
          * Uncomment to add constants which can be used in annotations
