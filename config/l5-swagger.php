c882f984236ffeaebe4bed76b223ce9c3ec032fb
Ashish Khokhar <761541+ashish-khokhar@users.noreply.github.com>
2021-10-18 16:58:30 +1030

    fixed var location in config (#411)
    * fixed var location in config
    
    * fixed route related bug

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -16,6 +16,11 @@ return [
             ],
             'paths' => [
                 /*
+                 * Edit to include full URL in ui for assets
+                */
+                'use_absolute_path' => env('L5_SWAGGER_USE_ABSOLUTE_PATH', true),
+
+                /*
                  * File name of the generated json documentation file
                 */
                 'docs_json' => 'api-docs.json',
@@ -88,11 +93,6 @@ return [
              * Edit to set path where swagger ui assets should be stored
             */
             'swagger_ui_assets_path' => env('L5_SWAGGER_UI_ASSETS_PATH', 'vendor/swagger-api/swagger-ui/dist/'),
-
-            /*
-             * Edit to include full URL in ui for assets
-            */
-            'use_absolute_path' => env('L5_SWAGGER_USE_ABSOLUTE_PATH', true),
 
             /*
              * Absolute path to directories that should be exclude from scanning
