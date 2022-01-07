e44cc6ad56899e0e919f86950eebe5e454837905
Daniel Iheonu <64360587+danieliheonu@users.noreply.github.com>
2022-01-07 06:47:42 +0100

    Added Sanctum Authentication to securitySchemes closes #318 (#428)
    * added securityScheme for Sanctum authentication which is available in Laravel 8
    
    * changes the name from bearer_token to sanctum
    
    Co-authored-by: Daniel Iheonu <64360587+danieldutcum@users.noreply.github.com>

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -185,6 +185,12 @@ return [
                         ],
                     ],
                 ],
+                'sanctum' => [ // Unique name of security
+                    'type' => 'apiKey', // Valid values are "basic", "apiKey" or "oauth2".
+                    'description' => 'Enter token in format (Bearer <token>)',
+                    'name' => 'Authorization', // The name of the header or query parameter to be used.
+                    'in' => 'header', // The location of the API key. Valid values are "query" or "header".
+                ],
                 */
             ],
             'security' => [
