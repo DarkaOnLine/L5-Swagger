ea476df5e5d6134136395e8ebfba60b076562ad0
Zakhar <zakhar.huzenko@business-class.com>
2025-02-11 09:56:03 +0200

    Use an array of API definition in a Swagger UI view

--- src/Http/Controllers/SwaggerController.php
+++ src/Http/Controllers/SwaggerController.php
@@ -10,6 +10,7 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Request as RequestFacade;
 use Illuminate\Support\Facades\Response as ResponseFacade;
+use L5Swagger\ConfigFactory;
 use L5Swagger\Exceptions\L5SwaggerException;
 use L5Swagger\GeneratorFactory;
 
@@ -20,9 +21,17 @@ class SwaggerController extends BaseController
      */
     protected $generatorFactory;
 
-    public function __construct(GeneratorFactory $generatorFactory)
-    {
+    /**
+     * @var ConfigFactory
+     */
+    protected $configFactory;
+
+    public function __construct(
+        GeneratorFactory $generatorFactory,
+        ConfigFactory $configFactory
+    ) {
         $this->generatorFactory = $generatorFactory;
+        $this->configFactory    = $configFactory;
     }
 
     /**
@@ -113,14 +122,17 @@ class SwaggerController extends BaseController
         }
 
         $urlToDocs = $this->generateDocumentationFileURL($documentation, $config);
+        $urlsToDocs = $this->getAllDocumentationUrls();
         $useAbsolutePath = config('l5-swagger.documentations.'.$documentation.'.paths.use_absolute_path', true);
 
         // Need the / at the end to avoid CORS errors on Homestead systems.
         return ResponseFacade::make(
             view('l5-swagger::index', [
                 'documentation' => $documentation,
+                'documentationTitle' => $config['api']['title'] ?? $documentation,
                 'secure' => RequestFacade::secure(),
-                'urlToDocs' => $urlToDocs,
+                'urlToDocs' => $urlToDocs, // Is not used in the view, but still passed for backwards compatibility
+                'urlsToDocs' => $urlsToDocs,
                 'operationsSorter' => $config['operations_sort'],
                 'configUrl' => $config['additional_config_url'],
                 'validatorUrl' => $config['validator_url'],
@@ -172,5 +184,24 @@ class SwaggerController extends BaseController
             $fileUsedForDocs,
             $useAbsolutePath
         );
+    }
+
+    /**
+     * @return array<string, string> [title => url]
+     */
+    protected function getAllDocumentationUrls(): array
+    {
+        $documentations = array_keys(config('l5-swagger.documentations', []));
+
+        $urlsToDocs = [];
+
+        foreach ($documentations as $documentationName) {
+            $config = $this->configFactory->documentationConfig($documentationName);
+            $title = $config['api']['title'] ?? $documentationName;
+
+            $urlsToDocs[$title] = $this->generateDocumentationFileURL($documentationName, $config);
+        }
+
+        return $urlsToDocs;
     }
 }
