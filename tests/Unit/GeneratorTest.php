87f00dd7fb4b9e3421ce15fc409ef86f07343bac
Darius Matulionis <darius.matulionis@supermetrics.com>
2026-01-05 11:43:04 +0200

    update test and bump swagger-php to 5.7

--- tests/Unit/GeneratorTest.php
+++ tests/Unit/GeneratorTest.php
@@ -7,7 +7,12 @@ use L5Swagger\Exceptions\L5SwaggerException;
 use L5Swagger\Generator;
 use L5Swagger\GeneratorFactory;
 use L5Swagger\L5SwaggerServiceProvider;
+use OpenApi\Analysers\AttributeAnnotationFactory;
+use OpenApi\Analysers\DocBlockAnnotationFactory;
+use OpenApi\Analysers\ReflectionAnalyser;
 use OpenApi\OpenApiException;
+use OpenApi\Processors\AugmentParameters;
+use OpenApi\Processors\CleanUnmerged;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\TestDox;
 use Symfony\Component\Yaml\Parser;
@@ -188,7 +193,12 @@ class GeneratorTest extends TestCase
 
         $cfg['scanOptions'] = [
             'exclude' => [__DIR__.'/../storage/annotations/OpenApi/Clients'],
+            'analyser' => new ReflectionAnalyser([
+                new AttributeAnnotationFactory(),
+                new DocBlockAnnotationFactory(),
+            ]),
             'open_api_spec_version' => '3.1.0',
+            'processors' => [new AugmentParameters],
             'default_processors_configuration' => ['operationId' => ['hash' => false]],
         ];
 
